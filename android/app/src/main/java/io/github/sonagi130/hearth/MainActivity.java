9cc16138413beaa57fe2bee69d2ff515d20e0066
Hearth <hearth@local>
2026-09-19 04:54:43 +0000

    app: add WebView wrapper + CI build

---
+++ android/app/src/main/java/io/github/sonagi130/hearth/MainActivity.java
@@ -0,0 +1,50 @@
+package io.github.sonagi130.hearth;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    private WebView web;
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        web = new WebView(this);
+        setContentView(web);
+
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setAllowFileAccess(true);
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        web.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
+
+        web.loadUrl("https://sonagi130.github.io/hearth/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (web != null && web.canGoBack()) {
+            web.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
